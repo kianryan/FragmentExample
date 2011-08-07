ad00092da96a5af600f4d52e9406e8ded2fc784c
Kian Ryan <kian@kian-ryans-macbook-pro.local>
2011-08-07 20:39:16 +0100

    Added callbacks to fragments to talk to each other. Changed layouts so textView's are visible.

--- src/com/orangetentacle/fragmentexample/Fragment1.java
+++ src/com/orangetentacle/fragmentexample/Fragment1.java
@@ -3,9 +3,11 @@ package com.orangetentacle.fragmentexample;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -18,6 +20,9 @@ import android.widget.TextView;
 public class Fragment1 extends Fragment {
 
     TextView textView;
+    Button button;
+
+    OnButton1ClickedListener mListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,13 +30,28 @@ public class Fragment1 extends Fragment {
     {
         View view = inflater.inflate(R.layout.fragment1, container, false);
         textView = (TextView) view.findViewById(R.id.textView1);
+        button = (Button) view.findViewById(R.id.button1);
+        button.setOnClickListener(clickListener);
         return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        mListener = (OnButton1ClickedListener)activity;
     }
 
     public void setText(String text)
     {
         textView.setText(text);
     }
+
+    public View.OnClickListener clickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            mListener.onButton1Clicked();
+        }
+    };
 
     public interface OnButton1ClickedListener {
         public void onButton1Clicked();
--- src/com/orangetentacle/fragmentexample/Fragment2.java
+++ src/com/orangetentacle/fragmentexample/Fragment2.java
@@ -1,10 +1,12 @@
 package com.orangetentacle.fragmentexample;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -16,21 +18,40 @@ import android.widget.TextView;
  */
 public class Fragment2 extends Fragment{
 
-    public TextView textView;
+    TextView textView;
+    Button button;
 
-        @Override
+    OnButton2ClickedListener mListener;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment2, container, false);
         textView = (TextView) view.findViewById(R.id.textView2);
+        button = (Button) view.findViewById(R.id.button2);
+        button.setOnClickListener(clickListener);
         return view;
     }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        mListener = (OnButton2ClickedListener)activity;
+    }
+
 
     public void setText(String text)
     {
         textView.setText(text);
     }
+
+    public View.OnClickListener clickListener = new View.OnClickListener() {
+        public void onClick(View view) {
+            mListener.onButton2Clicked();
+        }
+    };
 
     public interface OnButton2ClickedListener {
         public void onButton2Clicked();
